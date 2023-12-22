9967bdbb74cd88439cd710a4bddb4af5eaf8d318
vikram390 <112178932+vikram390@users.noreply.github.com>
2023-12-22 16:20:44 +0530

    Add files via upload

---
+++ scripts/afterinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Change to the deployment directory
+cd /usr/bin/pythonscript/
+
+# Perform any post-installation tasks
+echo "Running post-installation tasks..."
+# Add your commands here
+
+echo "Post-installation tasks completed."
+
---
+++ scripts/beforeinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Change to the deployment directory
+cd /usr/bin/pythonscript/
+
+# Perform any pre-installation tasks
+echo "Running pre-installation tasks..."
+# Add your commands here
+
+echo "Pre-installation tasks completed."
+
