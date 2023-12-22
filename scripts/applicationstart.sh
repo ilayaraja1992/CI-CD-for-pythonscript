3802c7b7776ee9cbd4eee163ff1088589afecea2
vikram390 <112178932+vikram390@users.noreply.github.com>
2023-12-22 16:20:14 +0530

    Create applicationstart.sh

---
+++ scripts/applicationstart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Change to the deployment directory
+cd /usr/bin/pythonscript/
+
+# Start your application
+echo "Starting the application..."
+python my_script.py  # Replace with the actual name of your Python script
+
+echo "Application started."
